ce1964c20aed420d4d36448a39ce562c6fe64a8c
j alfawadleh <j.alfawadleh@gmail.com>
2024-03-11 15:04:51 -0700

    Fixing getActivity

--- backend/controllers/activitiesController.js
+++ backend/controllers/activitiesController.js
@@ -47,14 +47,10 @@ const putActivity = asyncHandler(async (req, res) => {
 // @route   GET /api/activities/:id
 // @access  Private
 const getActivity = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const activity = await Activity.findById({ _id: req.params.id });
 
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    });
+  if (activity) {
+    res.json(activity);
   } else {
     res.status(404);
     throw new Error("User not found");
