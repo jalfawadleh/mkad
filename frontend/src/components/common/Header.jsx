3ece56930185edbd1ef94f6bebc2ab7edca9342b
j alfawadleh <j.alfawadleh@gmail.com>
2024-03-13 22:09:57 -0700

    Move header to the top

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -15,91 +15,63 @@ import { useContext } from "react";
 const Header = () => {
   const { user } = useContext(UserContext);
 
+  const headerLinks = (
+    <Nav
+      bg='dark'
+      variant='dark'
+      className='p-0 m-0 w-100 justify-content-between'
+    >
+      <Nav.Link href='/'>
+        <FaWindowClose />
+      </Nav.Link>
+      <LinkContainer to='settings'>
+        <Nav.Link>
+          <FaCog />
+        </Nav.Link>
+      </LinkContainer>
+      {user.type === "organisation" && (
+        <LinkContainer to='activities'>
+          <Nav.Link>
+            <FaFlag />
+          </Nav.Link>
+        </LinkContainer>
+      )}
+      <LinkContainer to='updates'>
+        <Nav.Link>
+          <FaBell />
+        </Nav.Link>
+      </LinkContainer>
+      <LinkContainer to='messages'>
+        <Nav.Link>
+          <FaEnvelope />
+        </Nav.Link>
+      </LinkContainer>
+      <LinkContainer to='search'>
+        <Nav.Link>
+          <FaSearch />
+        </Nav.Link>
+      </LinkContainer>
+    </Nav>
+  );
+
   return (
     <>
       <Navbar
         bg='dark'
-        className='d-none d-sm-block m-1 p-0'
+        className='d-none d-sm-block m-1 p-1'
         variant='dark'
-        style={{
-          position: "absolute",
-          top: 0,
-          right: 0,
-          zIndex: 1,
-          borderRadius: 10,
-        }}
+        style={{ borderRadius: 10, maxWidth: "300px" }}
       >
-        <Nav bg='dark' variant='dark' className='p-0 m-0 flex-column'>
-          <Nav.Link href='/'>
-            <FaWindowClose />
-          </Nav.Link>
-          <LinkContainer to='/settings'>
-            <Nav.Link>
-              <FaCog />
-            </Nav.Link>
-          </LinkContainer>
-
-          {user.type === "organisation" && (
-            <LinkContainer to='/activities'>
-              <Nav.Link>
-                <FaFlag />
-              </Nav.Link>
-            </LinkContainer>
-          )}
-          <LinkContainer to='/updates'>
-            <Nav.Link>
-              <FaBell />
-            </Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/messages'>
-            <Nav.Link>
-              <FaEnvelope />
-            </Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/search'>
-            <Nav.Link>
-              <FaSearch />
-            </Nav.Link>
-          </LinkContainer>
-        </Nav>
+        {headerLinks}
       </Navbar>
       <div
         style={{
           position: "absolute",
           bottom: 0,
-          width: "100%",
-          padding: 0,
-          margin: 0,
         }}
       >
-        <Navbar bg='dark' className='p-0 m-0 d-block d-sm-none ' variant='dark'>
-          <Nav justify bg='dark' variant='dark' className='p-0 m-0'>
-            <LinkContainer to='/settings'>
-              <Nav.Link>
-                <FaCog />
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/activities'>
-              <Nav.Link>
-                <FaFlag />
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/updates'>
-              <Nav.Link>
-                <FaBell />
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/messages'>
-              <Nav.Link>
-                <FaEnvelope />
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/'>
-              <Nav.Link>
-                <FaSearch />
-              </Nav.Link>
-            </LinkContainer>
-          </Nav>
+        <Navbar bg='dark' className='d.block d-sm-none p-0 m-0' variant='dark'>
+          {headerLinks}
         </Navbar>
       </div>
     </>
