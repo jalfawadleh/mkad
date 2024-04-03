cbdd9d9d07eab1ea255bb450993de0e798b4d8ac
j alfawadleh <j.alfawadleh@gmail.com>
2024-04-03 04:31:11 -0700

    Better interface for Activities

--- frontend/src/screens/ScreenActivities.jsx
+++ frontend/src/screens/ScreenActivities.jsx
@@ -17,17 +17,25 @@ const ScreenActivities = () => {
   const { user } = useContext(UserContext);
 
   const [activities, setActivities] = useState([]);
+  const [managedActivities, setManagedActivities] = useState([]);
+
   const [folded, setFolded] = useState(false);
 
   const getActivities = async () => {
-    try {
-      const { data } = await axios.get("/activities");
-      setActivities(data);
-    } catch (error) {
-      error?.response?.data?.message &&
-        toast.error(error?.response.data.message);
-      error?.response?.status > 499 && toast.error("Something went wrong");
-    }
+    await axios
+      .get("/activities")
+      .then((res) =>
+        res.data.map((a) =>
+          a.createdBy._id === user._id
+            ? setManagedActivities((prev) => [...prev, a])
+            : setActivities((prev) => [...prev, a])
+        )
+      )
+      .catch((error) => {
+        error?.response?.data?.message &&
+          toast.error(error?.response.data.message);
+        error?.response?.status > 499 && toast.error("Something went wrong");
+      });
   };
 
   useEffect(() => {
@@ -62,12 +70,33 @@ const ScreenActivities = () => {
 
   return (
     <>
+      {user.type === "organisation" && (
+        <ChocolateBar>
+          <span className='p-0 m-0' onClick={() => setFolded(!folded)}>
+            <IconFold color={folded ? "white" : "gray"} />
+          </span>
+          <div className='p-auto m-auto h5 text-center'>Manage Activities</div>
+          {user.type === "organisation" && <IconAddLink />}
+        </ChocolateBar>
+      )}
+
+      {user.type === "organisation" &&
+        (managedActivities.length
+          ? !folded && <ListManageActivities items={managedActivities} />
+          : !folded && (
+              <ChocolateBar>
+                <Icon>
+                  <IconExclamation color='white' />
+                </Icon>
+                <span className='p-auto m-auto'>No Activities Created</span>
+              </ChocolateBar>
+            ))}
+
       <ChocolateBar>
         <span className='p-0 m-0' onClick={() => setFolded(!folded)}>
           <IconFold color={folded ? "white" : "gray"} />
         </span>
-        <div className='p-auto m-auto h5 text-center'>Activities </div>
-        {user.type === "organisation" && <IconAddLink />}
+        <div className='p-auto m-auto h5 text-center'>Activities</div>
       </ChocolateBar>
 
       {activities.length
@@ -77,10 +106,7 @@ const ScreenActivities = () => {
               <Icon>
                 <IconExclamation color='white' />
               </Icon>
-              <span className='p-auto m-auto'>
-                No Activities joined{" "}
-                {user.type === "member" ? " yet" : " or created yet"}
-              </span>
+              <span className='p-auto m-auto'>No Activities joined yet</span>
             </ChocolateBar>
           )}
 
