bdbc751a17d51bd795e158c2ee6e775180923f63
j alfawadleh <j.alfawadleh@gmail.com>
2024-04-07 19:15:32 -0700

    creating online component for Activities

---
+++ frontend/src/components/common/ManageOnline.jsx
@@ -0,0 +1,90 @@
+import { Link } from "react-router-dom";
+
+const ManageOnline = ({ editing = false, parent, setParent }) => {
+  const online = parent.online;
+
+  return (
+    <>
+      {editing && (
+        <>
+          <div className='p-1 m-1 hstack gap-2'>
+            <input
+              type='checkbox'
+              className='btn-check mb-3'
+              id='online'
+              autoComplete='off'
+              checked={online}
+              onChange={(e) =>
+                setParent((prev) => ({
+                  ...prev,
+                  online: { value: e.target.checked, link: "" },
+                }))
+              }
+            />
+            <label
+              className='btn btn-outline-success '
+              htmlFor='online'
+              style={{ width: 140 }}
+            >
+              Online
+            </label>
+            {online.value && <div className='m-auto'>Put the link below</div>}
+          </div>
+          {online.value && (
+            <div className='form-floating m-1 p-1'>
+              <input
+                type='text'
+                className='form-control'
+                id='link'
+                placeholder='Link to event'
+                name='link'
+                value={location.link}
+                onChange={(e) =>
+                  setParent((prev) => ({
+                    ...prev,
+                    location: { value: true, link: e.target.value },
+                  }))
+                }
+              />
+              <label htmlFor='link'>Link</label>
+            </div>
+          )}
+          <hr className='m-1' />
+        </>
+      )}
+
+      {/* Viewing */}
+      {!editing && online.value && (
+        <>
+          <div className='p-1 m-1 w-100 d-flex justofy-contents-between'>
+            <div className='p-1 my-auto text-center'>ONLINE</div>
+
+            <Link
+              to={location.link}
+              target='_blank'
+              className='p-1 m-auto text-center link-underline link-underline-opacity-0 border border-1 border-primary text-primary'
+            >
+              Open Link
+            </Link>
+            <div
+              role='button'
+              className='p-1 m-auto text-center border border-1 border-primary text-primary'
+              onClick={() => {
+                navigator.clipboard.writeText(online.link);
+              }}
+            >
+              Copy Link
+            </div>
+          </div>
+
+          <div className='p-1 m-1'>Link</div>
+          <div className='p-2 my-2 small'>{online.link}</div>
+
+          <hr className='m-1' />
+        </>
+      )}
+    </>
+  );
+};
+
+export default ManageOnline;
