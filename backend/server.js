abe4b9e1d074cb85ab3821f21f25563c23d4e6af
j alfawadleh <j.alfawadleh@gmail.com>
2024-04-14 00:54:37 -0700

    Enable discussion

--- backend/server.js
+++ backend/server.js
@@ -80,7 +80,6 @@ app.use(errorHandler);
 
 const server = createServer(app);
 
-/*
 const io = new Server(server, {
   cors: {
     origin: "https://l:3000",
@@ -145,8 +144,6 @@ io.on("connection", (socket) => {
     console.log("disconnected: user: ", socket.user.name);
   });
 });
-
-*/
 
 // starting the socket.io and express servers on the same port
 server.listen(port, () => console.log(`Server started on port ${port}`));
