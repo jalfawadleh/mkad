bd0410dd45c044fdc6e7c0c821ce875e01bb5b10
j alfawadleh <j.alfawadleh@gmail.com>
2024-04-15 13:05:18 -0700

    Add user Edit

--- frontend/src/screens/ScreenDashboard.jsx
+++ frontend/src/screens/ScreenDashboard.jsx
@@ -6,13 +6,22 @@ import { UserContext } from "../store";
 import ListManagedActivities from "../components/dashboard/ListManagedActivities";
 import ListJoinedActivities from "../components/dashboard/ListJoinedActivities";
 import ListJoinedOrganisations from "../components/dashboard/ListJoinedOrganisations";
+import { Bar } from "../components/common/Wrappers";
+import { AvatarCustomLink } from "../components/common/Icons";
 
 const ScreenDashboard = () => {
   const { user } = useContext(UserContext);
 
   return (
     <>
-      {user.type === "organisation" && <ListManagedActivities />}
+      {user.type === "organisation" ? (
+        <ListManagedActivities />
+      ) : (
+        <Bar>
+          <AvatarCustomLink name={user.name} />
+          <span className='p-auto m-auto'>{user.name}</span>
+        </Bar>
+      )}
 
       <ListJoinedOrganisations />
 
