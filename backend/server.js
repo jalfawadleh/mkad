0f6d236e3fdf33ad44e16b6edf3536657b875e6f
j alfawadleh <j.alfawadleh@gmail.com>
2024-05-08 00:44:36 -0700

    testing

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.use("/api/updates", updates);
 app.use("/api/invites", invites);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("/", (req, res) =>
+app.get("*", (req, res) =>
   res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
 );
 
